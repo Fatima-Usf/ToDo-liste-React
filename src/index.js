b3bdf64fdcbdddfa4447d4a1cd5b20fab5cfe656
Fatima-Usf <f.ucf.eva@gmail.com>
2020-01-31 15:39:18 +0100

    initilisation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// rendre dans notre dom le component App dans la div qui a l'id root
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
